1217afb6d2b57b040baca954121001c4097ab4d1
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2018-09-29 13:37:41 +0800

    comments

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,34 @@ import {
 } from './utils'
 
 export type OptionsType = {
-  filter?: Function,
+  /**
+   * A function taking DOM node as argument. Should return `true`
+   * if passed node should be included in the output. Excluding
+   * node means excluding it's children as well.
+  */
+  filter?: (domNode: HTMLElement) => boolean,
   width?: number,
   height?: number,
   style?: Object,
+  /**
+   * A number between `0` and `1` indicating image quality (e.g. 0.92 => 92%)
+   * of the JPEG image.
+  */
   quality?: number,
+  /**
+   * A string value for the background color, any valid CSS color value.
+  */
   backgroundColor?: string,
+  /**
+   * Set to `true` to append the current time as a query string to URL
+   * requests to enable cache busting.
+  */
   cacheBust?: boolean,
+  /**
+   * A data URL for a placeholder image that will be used when fetching
+   * an image fails. Defaults to an empty string and will render empty
+   * areas for failed images.
+  */
   imagePlaceholder?: string,
 }
 
