9bab9fbfa809ae345fe3e4ec6ac2c0612bd3028f
henri.kovaci <henri.kovaci@servicenow.com>
2021-01-17 21:12:09 +0100

    feat: ✨ adds support for slotted custom elements

--- test/spec/resources/web-components-sloted/sc.js
+++ test/spec/resources/web-components-sloted/sc.js
@@ -2,9 +2,9 @@ customElements.define(
   'my-paragraph',
   class extends HTMLElement {
     constructor() {
-      super()
-      const template = document.getElementById('my-paragraph')
-      const templateContent = template.content
+      super();
+      const template = document.getElementById('my-paragraph');
+      const templateContent = template.content;
       this.attachShadow({ mode: 'open' }).appendChild(
         templateContent.cloneNode(true),
       )
