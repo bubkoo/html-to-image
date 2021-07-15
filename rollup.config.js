67be1166d6db8345b740dc57bdba9b55fcab50e4
Gordon Smith <GordonSmith@users.noreply.github.com>
2021-07-15 07:24:57 +0100

    fix: 🐛  UMD bundle is missing local source dependencies (#138)
    Current UMD bundle contains several invalid "require" statements
    (causing it to fail when used with an AMD loader):
      * require("./cloneNode")
      * require("./embedImages")
      * require("./embedWebFonts")
      * require("./createSvgDataURL")
      * require("./applyStyleWithOptions")
      * require("./util");
    
    Rather than bundling the "commonjs" generated sources, this change lets rollup bundle via ES6
    modules (which it prefers).
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
-import { terser } from "rollup-plugin-terser";
-import commonjs from "@rollup/plugin-commonjs";
-import filesize from "rollup-plugin-filesize";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import { terser } from 'rollup-plugin-terser'
+import commonjs from '@rollup/plugin-commonjs'
+import filesize from 'rollup-plugin-filesize'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
 
 export default {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: [
     {
-      name: "htmlToImage",
-      format: "umd",
-      file: "dist/html-to-image.js",
+      name: 'htmlToImage',
+      format: 'umd',
+      file: 'dist/html-to-image.js',
       sourcemap: true,
     },
   ],
   plugins: [
-    typescript({ declaration: false }),
+    typescript({ declaration: false, module: 'ES6' }),
     resolve(),
     commonjs(),
     terser(),
     filesize(),
   ],
-};
+}
