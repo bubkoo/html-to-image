c22328e98565d1492e7cb0e9116f3abc0a07c401
ShaMan123 <shacharnen@gmail.com>
2022-02-02 11:44:43 +0200

    fix(cloneNode): 🐛 `checked` attr

--- src/cloneNode.ts
+++ src/cloneNode.ts
@@ -98,6 +98,7 @@ function cloneInputValue<T extends HTMLElement>(nativeNode: T, clonedNode: T) {
 
   if (nativeNode instanceof HTMLInputElement) {
     clonedNode.setAttribute('value', nativeNode.value)
+    clonedNode.setAttribute('checked', nativeNode.checked ? 'true' : '')
   }
 }
 
