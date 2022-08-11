8d5f76a04d44ebdfd563614097f57072a97872fa
bubkoo <bubkoo.wy@gmail.com>
2022-08-11 17:02:33 +0800

    chore: 🔧 use common config files of bubkoo

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,6 @@
-import { terser } from 'rollup-plugin-terser'
-import commonjs from '@rollup/plugin-commonjs'
-import filesize from 'rollup-plugin-filesize'
-import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
+import config from '@bubkoo/rollup-config'
 
-export default {
-  input: './src/index.ts',
+export default config({
   output: [
     {
       name: 'htmlToImage',
@@ -14,11 +9,4 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [
-    typescript({ declaration: false, module: 'ES6' }),
-    resolve(),
-    commonjs(),
-    terser(),
-    filesize(),
-  ],
-}
+})
