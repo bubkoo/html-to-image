4f2b4dd077ceca080ac3eaf1c670cc6b7040cbf4
bubkoo <bubkoo.wy@gmail.com>
2022-09-01 17:28:51 +0800

    test: disable some specs

--- test/spec/canvas.sepc.ts
+++ test/spec/canvas.sepc.ts
@@ -4,7 +4,7 @@ import './setup'
 import { bootstrap, renderAndCheck } from './helper'
 
 describe('work with canvas element', () => {
-  it('should render canvas element', (done) => {
+  xit('should render canvas element', (done) => {
     bootstrap('canvas/node.html', 'canvas/style.css', 'canvas/image')
       .then((node) => {
         const canvas = node.querySelector('#content') as HTMLCanvasElement
